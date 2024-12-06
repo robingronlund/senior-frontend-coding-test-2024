9be58cb1eb0cdfaeff9f304a4b4cab67f8890f5b
Robin Grönlund <robin.gronlund@protonmail.com>
2024-12-06 15:08:00 +0100

    feat: add movie interface

---
+++ src/features/movies/types/movies.types.ts
@@ -0,0 +1,38 @@
+export interface Movie {
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Ratings: Rating[];
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  Type: string;
+  DVD: string;
+  BoxOffice: string;
+  Production: string;
+  Website: string;
+  Response: string;
+}
+
+interface Rating {
+  Source: string;
+  Value: string;
+}
+
+export interface SearchResult {
+  Search: Pick<Movie, 'Title' | 'Year' | 'imdbID' | 'Type' | 'Poster'>[];
+  totalResults: string;
+  Response: string;
+}
