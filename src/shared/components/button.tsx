19b5f58f1df11ea2e6b0ba0dce927484b8986890
Robin Grönlund <robin.gronlund@protonmail.com>
2024-12-06 15:11:50 +0100

    feat: adds components

---
+++ src/shared/components/button.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+  padding: 0.5rem 1rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f0f0f0;
+    border-color: #999;
+  }
+`;
