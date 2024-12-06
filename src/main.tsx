f7e175ff3a42562b8f59aeb081505262d054630c
Robin Grönlund <robin.gronlund@protonmail.com>
2024-12-06 15:11:18 +0100

    chore: use SearchProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { SearchProvider } from './features/movies/context/search.provider.tsx';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <SearchProvider>
+        <App />
+      </SearchProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
