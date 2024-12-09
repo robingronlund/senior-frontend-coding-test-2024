41f51fd1722524301e70a3a1264e4aafaeaeaaa3
Robin Grönlund <robin.gronlund@protonmail.com>
2024-12-09 19:43:00 +0100

    style: adjust base styling and button styling

--- src/shared/components/button.tsx
+++ src/shared/components/button.tsx
@@ -2,13 +2,14 @@ import styled from '@emotion/styled';
 
 export const Button = styled.button`
   padding: 0.5rem 1rem;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  border: 1px solid hsl(0, 0%, 27.1%);
+  border-radius: 8px;
   background-color: #fff;
+  color: hsl(0, 0%, 27.1%);
   cursor: pointer;
 
   &:hover {
-    background-color: #f0f0f0;
-    border-color: #999;
+    border-color: hsl(0, 0%, 50%);
+    color: hsl(0, 0%, 50%);
   }
 `;
