d5c49c89567c49ba565aee4464923069ff9a5fa9
Robin Grönlund <robin.gronlund@protonmail.com>
2024-12-09 15:25:28 +0100

    chore: move queryClient and Providers into App.tsx from main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import { RouterProvider } from 'react-router';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MoviesProvider } from './features/movies/context/movies.provider';
 import { router } from './router';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 export const App = () => {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MoviesProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </MoviesProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { SearchProvider } from './features/movies/context/search.provider.tsx';
 import './index.css';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <SearchProvider>
-        <App />
-      </SearchProvider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
